796d0882a50ba347210b576a93b9808c8fbafa71
Delmy Melendez <delmymm93@gmail.com>
2021-02-16 14:28:09 -0700

    api.js file re-worked.

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require("express");
-const path = require("path");
-
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3001;
+
+app.use(logger("dev"));
+
+require("./routes/html")(app);
+require("./routes/api")(app);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT);
+});
